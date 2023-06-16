b97f7f1765a59491df9af689f6eb52f2720b691f
Mushfiq <mushfiqhumayoon@gmail.com>
2023-06-16 22:56:23 +0530

    Initial Commit

---
+++ MVVMStarter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMStarter
+//
+//  Created by Mushfiq Humayoon on 16/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
